63eab031387563d2aea90b1269978c660e8e7b7b
Ubaydah <abdulwasiuubaydah@gmail.com>
2022-02-28 12:08:00 +0100

    added mptt library

--- ecommerce/inventory/models.py
+++ ecommerce/inventory/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey, TreeManyToManyField
 
-# Create your models here.
+
+class Category(MPTTModel):
+    
+    """
+    Inventory Category table implemented with MPTT
+    """
+
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        unique=False,
+        blank=False,
+        verbose_name=_("category_name"),
+        help_text=_("format: required, max-100"),
+    )
+
